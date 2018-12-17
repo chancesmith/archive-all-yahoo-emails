e7f816978184c69687f8717c22bb9254541593b8
Chance Smith <chancesmithb@gmail.com>
2018-12-16 21:14:50 -0600

    move selectors into vars

--- main.js
+++ main.js
@@ -1,28 +1,20 @@
+const selectAllBtn = "#mail-app-component > div.W_6D6F.D_F > div > div.en_0 > div > div:nth-child(1) > div > div > ul > li:nth-child(1) > span > button";
+const archiveBtn = "#mail-app-component > div.W_6D6F.D_F > div > div.en_0 > div > div.D_F.em_N.gl_C > ul > li:nth-child(1) > div > button";
+const emailListItem = "#mail-app-component > div.W_6D6F.D_F > div > div.D_F.H_6D6F.ab_FT.em_N.M_0.p_R > div > div.W_6D6F.H_6D6F > div > div.p_R.iy_h.iz_A.H_6D6F.W_6D6F.X_6Fd5.N_6Fd5.k_w > div.hd_n.P_0.M_0.p_R > ul:nth-child(1) > li:nth-child(3)";
+
 function archiveAvailEmails() {
   // click SelectAll button
-  document
-    .querySelector(
-      "#mail-app-component > div.W_6D6F.D_F > div > div.en_0 > div > div:nth-child(1) > div > div > ul > li:nth-child(1) > span > button"
-    )
-    .click();
+  document.querySelector(selectAllBtn).click();
   
   // click Archive button as soon as possible
   setTimeout(function() {
-    document
-      .querySelector(
-        "#mail-app-component > div.W_6D6F.D_F > div > div.en_0 > div > div.D_F.em_N.gl_C > ul > li:nth-child(1) > div > button"
-      )
-      .click();
+    document.querySelector(archiveBtn).click();
   }, 700);
 }
 
 // check every 6s if there are more emails to archive
 setInterval(function() {
-  if (
-    document.querySelector(
-      "#mail-app-component > div.W_6D6F.D_F > div > div.D_F.H_6D6F.ab_FT.em_N.M_0.p_R > div > div.W_6D6F.H_6D6F > div > div.p_R.iy_h.iz_A.H_6D6F.W_6D6F.X_6Fd5.N_6Fd5.k_w > div.hd_n.P_0.M_0.p_R > ul:nth-child(1) > li:nth-child(3)"
-    )
-  ) {
+  if ( document.querySelector(emailListItem) ) {
     archiveAvailEmails();
   }
 }, 6000);
